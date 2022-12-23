25a660c145e852ad360b0fc986bdd54b292cc81d
Luciano Sampaio <lsampaioweb@gmail.com>
2022-12-23 12:09:47 -0300

    Ordered the attributes by its importance.

--- variables-network.tf
+++ variables-network.tf
@@ -1,10 +1,10 @@
 variable "networks" {
   description = "Network configuration for the VM."
   type = map(object({
-    # Required. Network Card Model. The virtio model provides the best performance with very low CPU overhead. If your guest does not support this driver, it is usually best to use e1000. Options: e1000, e1000-82540em, e1000-82544gc, e1000-82545em, i82551, i82557b, i82559er, ne2k_isa, ne2k_pci, pcnet, rtl8139, virtio, vmxnet3.
-    model = string
     # Bridge to which the network device should be attached. The Proxmox VE standard bridge is called vmbr0.
     bridge = string
+    # Required. Network Card Model. The virtio model provides the best performance with very low CPU overhead. If your guest does not support this driver, it is usually best to use e1000. Options: e1000, e1000-82540em, e1000-82544gc, e1000-82545em, i82551, i82557b, i82559er, ne2k_isa, ne2k_pci, pcnet, rtl8139, virtio, vmxnet3.
+    model = string
     # The VLAN tag to apply to packets on this device. -1 disables VLAN tagging.
     tag = optional(number)
     # Whether to enable the Proxmox firewall on this network device. Defaults to false.
@@ -16,8 +16,8 @@ variable "networks" {
   default = {
     "01" = {
       # WAN
-      model    = "virtio"
       bridge   = "vmbr0"
+      model    = "virtio"
     }
   }
 }
