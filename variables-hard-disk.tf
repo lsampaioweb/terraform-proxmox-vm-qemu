7cf7b2b0f239733752e200c26a635d0dab2d7c8e
Luciano Sampaio <lsampaioweb@gmail.com>
2023-02-14 23:45:22 -0300

    Fixed the indentation.

--- variables-hard-disk.tf
+++ variables-hard-disk.tf
@@ -6,7 +6,7 @@ variable "disks" {
     size      = string # Required. The size of the created disk, format must match the regex \\d+[GMK], where G, M, and K represent Gigabytes, Megabytes, and Kilobytes respectively.
     format    = string # The format of the file backing the disk. Options: raw, cow, qcow, qed, qcow2, vmdk and cloop. Defaults to raw.
     cache     = string # The drive's cache mode. Options: none, directsync, unsafe, writeback and writethrough. Defaults to none.
-    backup    = bool # Whether the drive should be included when making backups. Defaults to false.
+    backup    = bool   # Whether the drive should be included when making backups. Defaults to false.
     iothread  = number # Whether to use iothreads for this drive. Only effective with a disk of type virtio, or scsi when the emulated controller type (scsihw top level block argument) is virtio-scsi-single. Defaults to 1.
     replicate = number # Whether the drive should considered for replication jobs. Defaults to 0.
     ssd       = number # Whether to expose this drive as an SSD, rather than a rotational hard disk. Defaults to 1.
